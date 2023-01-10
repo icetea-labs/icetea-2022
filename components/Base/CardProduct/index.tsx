9253d69721c3db859788ccb0498ed3c249f53665
Ha Nguyen <ha.nguyen@icetea.io>
2023-01-10 09:47:14 +0700

    update product card

--- components/Base/CardProduct/index.tsx
+++ components/Base/CardProduct/index.tsx
@@ -1,5 +1,6 @@
 import clsx from "clsx"
 import Image from "next/image"
+import Link from "next/link"
 import { ProductTypes, SubProductTypes } from "../../../utils/constants"
 import styles from "./cardProduct.module.scss"
 
@@ -9,18 +10,23 @@ type CardProductTypes = {
 
 const CardProduct = (props: CardProductTypes) => {
   const { dataCard } = props
-  const { bgUri, description, imageUri, title } = dataCard
+  const { bgUri, description, imageUri, title, url } = dataCard
 
   return (
     <div className="flex flex-col">
-      <div
+      <Link
+        href={url}
+        target="_blank"
         className={clsx(styles.imgHover, "flex justify-center items-end w-[550px] h-[550px]")}
         style={{
-          background: `${bgUri} center center no-repeat`
+          background: `url(${bgUri}) center center no-repeat`,
+          backgroundSize: "contain"
         }}
       >
-        <Image src={imageUri} alt="" className="" />
-      </div>
+        <Image src={imageUri} alt="" className="max-h-[400px] object-contain" />
+      </Link>
+      <p className="text-28/36 font-bevn600 mt-6">{title}</p>
+      <p className="text-teaGray mt-2 max-w-[360px]">{description}</p>
     </div>
   )
 }
